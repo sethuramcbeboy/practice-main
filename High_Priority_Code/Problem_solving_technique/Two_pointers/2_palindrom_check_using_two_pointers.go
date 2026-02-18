685536681ab01ab4ee907e0781de813e8d59c121
SETHURAM B <sethuram450@gmail.com>
2026-02-18 19:59:55 +0530

    Update 18/2/2026 2

---
+++ High_Priority_Code/Problem_solving_technique/Two_pointers/2_palindrom_check_using_two_pointers.go
@@ -0,0 +1,20 @@
+package twopointers
+
+import "fmt"
+
+func Palindrome_check_using_two_pointers(s string) bool {
+	s_rune := []rune(s)
+	j := len(s_rune) - 1
+	for i := 0; i < len(s_rune)/2; i++ {
+		if s[i] != s[j] {
+			return false
+		}
+		j--
+	}
+	return true
+}
+
+func main_s() {
+	s := "abcdbbdcba"
+	fmt.Println("String is palindrome", Palindrome_check_using_two_pointers(s))
+}
